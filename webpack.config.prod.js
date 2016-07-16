adee540e7cb4be8a8e272125c0686c07a9a0e7ad
vamsiampolu <vamsideepak03@gmail.com>
2016-07-17 08:22:19 +1000

    [WIP] Added loader config for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,5 +11,53 @@ var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
 
 var config = {
   entry: constants.PROD_ENTRY,
-  output: constants.PROD_OUTPUT
+  output: constants.PROD_OUTPUT,
+  module: {
+    loaders: [
+      {
+        test: constants.REGEX.CSS,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss')
+      },
+      {
+        test: constants.REGEX.IMAGE,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack'
+        ]
+      },
+      {
+        test: constants.REGEX.SVG,
+        loader: ['svg-sprite?name=[name].[hash]', 'image-webpack']
+      }
+    ]
+  },
+  imageWebpackLoader: {
+    imagemin: {
+      optimizationLevel: 7,
+      interlaced: false
+    },
+    pngquant: {
+      quality: '65-90',
+      speed: 4
+    },
+    svgo: {
+      plugins: [
+        {
+          removeViewBox: false
+        },
+        {
+          removeEmptyAttrs: false
+        }
+      ]
+    }
+  },
+  devtool: 'source-map',
+// plugins: [
+//   new CommonsChunkPlugin(chunks),
+//   new ExtractTextPlugin('[name][chunkhash].css'),
+//   new PurifyCssPlugin(purifyConfig),
+//   new UglifyJsPlugin(uglifyConfig),
+//   new VisualizerPlugin(),
+//   new CleanPlugin([DIST_PATH], cleanPluginConfig)
+// ]
 }
