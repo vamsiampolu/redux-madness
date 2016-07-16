e01e813c56d2a3baccc7111879dd49f9435be532
vamsiampolu <vamsideepak03@gmail.com>
2016-07-17 05:41:08 +1000

    Adds files for webpack config per env

---
+++ webpack.config.constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DEV: 'DEVELOPMENT',
+  PROD: 'PRODUCTION',
+  TEST: 'TESTING'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
+/*
+  Notes:
+
+    + I need to figure out what bits I need to retrieve from  dotenv
+      and what bits of config can be pushed to a constants file...
+
+    + It goes without saying that I will have a seperate config for
+    development, production and testing(if I can)
+ */
+
 var webpack = require('webpack')
 var merge = require('webpack-merge')
+var validator = require('webpack-validator')
+var constants = require('./webpack.config.constants')
+var commonConfig = require('./webpack.config.common')
+
+require('dotenv').config()
+var env = process.env.NODE_ENV
 var config = {}
+switch (env) {
+  case constants.DEV:
+    var devConfig = require('./webpack.config.dev')
+    config = merge(commonConfig, devConfig)
+    break
+}
+
+module.exports = validator(config)
